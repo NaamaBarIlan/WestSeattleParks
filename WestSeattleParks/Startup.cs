44bb96a36c69470bbcd983247730e8d0d2ece3c1
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-03-21 17:44:44 -0700

    Added file structure and XUnit tests

--- WestSeattleParks/Startup.cs
+++ WestSeattleParks/Startup.cs
@@ -28,6 +28,8 @@ namespace WestSeattleParks
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>

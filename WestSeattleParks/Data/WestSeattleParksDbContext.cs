9db993cf6edc5506c77486392dd65c55af6b3b61
Naama Bar-Ilan <barilan.naama@gmail.com>
2021-03-22 20:03:58 -0700

    Updated DbContext and packages

---
+++ WestSeattleParks/Data/WestSeattleParksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WestSeattleParks.Data
+{
+    public class WestSeattleParksDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+
+
+}
